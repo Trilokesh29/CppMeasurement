9d4365dc40fe8a9a6a8c06f5d437d0b4a0d7e83a
pradhant <trilokeshpradhan@gmail.com>
2021-12-24 22:35:58 +0100

    fixup! Update c-cpp.yml

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,7 @@ function main
     readonly SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
     mkdir ${SCRIPT_DIR}/build
     cd ${SCRIPT_DIR}/build
-    cmake ../test/.
+    cmake ../.
     make -j4
     cd bin
     ./ScopedMeasurementTest

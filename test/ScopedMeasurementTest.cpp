0c6b3912c55bb440681d97e72ea28f31d9a66e11
Trilokesh Pradhan <trilokeshranjan.pradhan@tomtom.com>
2021-12-24 15:25:20 +0100

    Creating a single header implementation for easier benchmarking

--- test/ScopedMeasurementTest.cpp
+++ test/ScopedMeasurementTest.cpp
@@ -4,20 +4,20 @@
 
 TEST(ScopedMeasurementTest, AutomaticScopeComplete) {
   testing::internal::CaptureStdout();
-  { CScopedMeasurement m("AutomaticScopeComplete"); }
+  { benchmark::timer m("AutomaticScopeComplete"); }
   const std::string output = testing::internal::GetCapturedStdout();
   ASSERT_EQ(false, output.empty());
 }
 
 TEST(ScopedMeasurementTest, ManualScopeReduction) {
   testing::internal::CaptureStdout();
-  auto m1 = std::make_unique<CScopedMeasurement>("ManualScopeReduction");
+  auto m1 = std::make_unique<benchmark::timer>("ManualScopeReduction");
   m1.reset();
   const std::string output1 = testing::internal::GetCapturedStdout();
   ASSERT_EQ(false, output1.empty());
 
   testing::internal::CaptureStdout();
-  auto m2 = std::make_unique<CScopedMeasurement>("ManualScopeReduction");
+  auto m2 = std::make_unique<benchmark::timer>("ManualScopeReduction");
   const std::string output2 = testing::internal::GetCapturedStdout();
 
   ASSERT_EQ(true, output2.empty());
@@ -25,14 +25,14 @@ TEST(ScopedMeasurementTest, ManualScopeReduction) {
 
 TEST(ScopedMeasurementTest, MeasureBeforeScopeCompletion) {
   testing::internal::CaptureStdout();
-  CScopedMeasurement m("MeasureBeforeScopeCompletion");
+  benchmark::timer m("MeasureBeforeScopeCompletion");
 
   auto fu = std::async(std::launch::async, []() {
     std::this_thread::sleep_for(std::chrono::seconds(3));
   });
 
   fu.get();
-  m.PrintTimeConsumed();
+  m.print_time_consumed();
   const std::string output = testing::internal::GetCapturedStdout();
   ASSERT_EQ(false, output.empty());
 }
@@ -41,7 +41,7 @@ TEST(ScopedMeasurementTest, TestMoveMeasurement) {
 
   testing::internal::CaptureStdout();
 
-  CScopedMeasurement m("MoveMeasurement");
+  benchmark::timer m("MoveMeasurement");
 
   std::this_thread::sleep_for(std::chrono::seconds(2));
 
@@ -50,10 +50,20 @@ TEST(ScopedMeasurementTest, TestMoveMeasurement) {
       std::this_thread::sleep_for(std::chrono::seconds(3));
     });
     fu.get();
-    measure.PrintTimeConsumed();
+    measure.print_time_consumed();
     const std::string output = testing::internal::GetCapturedStdout();
     ASSERT_EQ(false, output.empty());
   };
 
   func(std::move(m));
+}
+
+TEST(ScopedMeasurementTest, TestNanoMsFunc) {
+  benchmark::timer tm("NanoMsFunctionTest");
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+  const double elapsed = tm.get_elapsed_ns();
+  const auto expected_min =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(
+          std::chrono::seconds(3));
+  ASSERT_GT(elapsed, expected_min.count());
 }
